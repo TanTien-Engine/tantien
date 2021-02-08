f98101046fd3819a2c3c0fa4b760c2b2440a5579
zz <xzrunner@gmail.com>
2021-02-09 06:19:51 +0800

    [ADDED] ImageData funcs

--- src/modules/image/wrap_Image.cpp
+++ src/modules/image/wrap_Image.cpp
@@ -2,6 +2,8 @@
 #include "modules/image/ImageData.h"
 
 #include <gimg_import.h>
+#include <gimg_typedef.h>
+#include <gimg_export.h>
 
 #include <string>
 
@@ -30,8 +32,47 @@ static int image_data_finalize(void* data)
     return sizeof(tt::ImageData);
 }
 
-void image_data_trans()
+void w_ImageData_toRGB565()
 {
+    tt::ImageData* img = (tt::ImageData*)ves_toforeign(0);
+
+    int channels = 0;
+    switch (img->format)
+    {
+    case GPF_RGBA8:
+        channels = 4;
+        break;
+    case GPF_RGB:
+        channels = 3;
+        break;
+    default:
+        // unsupported format
+        return;
+    }
+
+    uint8_t* pixels = new uint8_t[img->width * img->height * 2];
+    for (int i = 0, n = img->width * img->height; i < n; ++i)
+    {
+        uint8_t r = img->pixels[i * channels];
+        uint8_t g = img->pixels[i * channels + 1];
+        uint8_t b = img->pixels[i * channels + 2];
+        uint16_t _r = uint16_t(r / 255.0f * 31) << 11;
+        uint16_t _g = uint16_t(g / 255.0f * 63) << 5;
+        uint16_t _b = uint16_t(b / 255.0f * 31);
+        uint16_t rgb = _r | _g | _b;
+        memcpy(&pixels[i * 2], &rgb, 2);
+    }
+
+    delete[] img->pixels;
+    img->pixels = pixels;
+    img->format = GPF_RGB565;
+}
+
+void w_ImageData_storeToFile()
+{
+    tt::ImageData* img = (tt::ImageData*)ves_toforeign(0);
+    const char* filepath = ves_tostring(1);
+    gimg_export(filepath, img->pixels, img->width, img->height, img->format, 0);
 }
 
 }
@@ -41,7 +82,8 @@ namespace tt
 
 VesselForeignMethodFn ImageBindMethod(const char* signature)
 {
-    if (strcmp(signature, "ImageData.trans()") == 0) return image_data_trans;
+    if (strcmp(signature, "ImageData.toRGB565()") == 0) return w_ImageData_toRGB565;
+    if (strcmp(signature, "ImageData.storeToFile(_)") == 0) return w_ImageData_storeToFile;
 
     return nullptr;
 }
