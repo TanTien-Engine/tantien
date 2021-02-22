ea265380e12a08b4e7b08a1064e51f0c71149b4a
zz <xzrunner@gmail.com>
2021-02-23 05:48:57 +0800

    [ADDED] press F5 to refresh scene

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -183,6 +183,9 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     } else if (key == GLFW_KEY_S && action == GLFW_PRESS && ctrl_pressed) {
         ves_pushstring("savetofile()");
         ves_call(0, 0);
+    } else if (key == GLFW_KEY_F5 && action == GLFW_PRESS) {
+        ves_pushstring("refresh()");
+        ves_call(0, 0);
     }
 }
 
