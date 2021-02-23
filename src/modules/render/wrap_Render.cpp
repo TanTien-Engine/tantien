9684a1387e254f67708ed166cf4ed59f8386823a
zz <xzrunner@gmail.com>
2021-02-24 05:59:24 +0800

    [ADDED] auto gen imports and props for shader uniforms

--- src/modules/render/wrap_Render.cpp
+++ src/modules/render/wrap_Render.cpp
@@ -12,7 +12,13 @@
 #include <unirender/ClearState.h>
 #include <unirender/PrimitiveType.h>
 #include <unirender/ShaderProgram.h>
+#include <unirender/Uniform.h>
 #include <shadertrans/ShaderTrans.h>
+
+#include <glslang/glslang/Public/ShaderLang.h>
+
+#include <spirv-cross/spirv.hpp>
+#include <spirv-cross/spirv_glsl.hpp>
 
 #include <assert.h>
 
@@ -217,6 +223,273 @@ void w_Render_clear()
     tt::Render::Instance()->Context()->Clear(clear);
 }
 
+std::string parse_spir_type(const spirv_cross::SPIRType& type)
+{
+    std::string ret = "unknown";
+
+	switch (type.basetype)
+	{
+	case spirv_cross::SPIRType::BaseType::Boolean:
+		if (type.columns == 1 && type.vecsize == 1) {
+			ret = "bool";
+		}
+		break;
+
+	case spirv_cross::SPIRType::BaseType::Int:
+		switch (type.vecsize) 
+		{
+		case 1: 
+			ret = "int";
+			break;
+		case 2: 
+			ret = "int2";
+			break;
+		case 3: 
+			ret = "int3";
+			break;
+		case 4: 
+			ret = "int4";
+			break;
+		}
+		break;
+
+	case spirv_cross::SPIRType::BaseType::Float:
+		switch (type.columns) 
+		{
+		case 1:
+			switch (type.vecsize) 
+			{
+			case 1: 
+				ret = "float";
+				break;
+			case 2: 
+				ret = "float2";
+				break;
+			case 3: 
+				ret = "float3";
+				break;
+			case 4: 
+				ret = "float4";
+				break;
+			}
+			break;
+
+		case 2:
+			if (type.vecsize == 2) {
+				ret = "mat2";
+			}
+			break;
+				
+		case 3:
+			if (type.vecsize == 3) {
+				ret = "mat3";
+			}
+			break;
+
+		case 4:
+			if (type.vecsize == 4) {
+				ret = "mat4";
+			}
+			break;
+		}
+		break;
+	}
+
+	return ret;
+}
+
+void get_shader_uniforms(EShLanguage stage, const std::vector<unsigned int>& shader,
+                         std::vector<std::pair<std::string, std::string>>& uniforms)
+{
+    spirv_cross::CompilerGLSL compiler(shader);
+    spirv_cross::ShaderResources resources = compiler.get_shader_resources();
+
+	//// get_work_group_size_specialization_constants
+	//auto entries = compiler.get_entry_points_and_stages();
+	//for (auto& e : entries)
+	//{
+	//	if (e.execution_model == spv::ExecutionModelGLCompute)
+	//	{
+	//		const auto& spv_entry = compiler.get_entry_point(e.name, e.execution_model);
+	//		m_props.insert({ "local_size_x", ShaderVariant(static_cast<int>(spv_entry.workgroup_size.x)) });
+	//		m_props.insert({ "local_size_y", ShaderVariant(static_cast<int>(spv_entry.workgroup_size.y)) });
+	//		m_props.insert({ "local_size_z", ShaderVariant(static_cast<int>(spv_entry.workgroup_size.z)) });
+	//		break;
+	//	}
+	//}
+
+	// uniforms
+    for (auto& resource : resources.uniform_buffers)
+    {
+        auto ubo_name = compiler.get_name(resource.id);;
+        spirv_cross::SPIRType type = compiler.get_type(resource.base_type_id);
+
+        auto member_count = type.member_types.size();
+        for (int i = 0; i < member_count; i++)
+        {
+            std::pair<std::string, std::string> unif;
+
+            unif.first = compiler.get_member_name(resource.base_type_id, i);
+            unif.second = parse_spir_type(compiler.get_type(type.member_types[i]));
+
+			size_t size = compiler.get_declared_struct_member_size(type, i);
+            size_t offset = compiler.type_struct_member_offset(type, i);
+
+			uniforms.push_back(unif);
+        }
+
+		uint32_t set = compiler.get_decoration(resource.id, spv::DecorationDescriptorSet);
+		uint32_t binding = compiler.get_decoration(resource.id, spv::DecorationBinding);		
+    }
+
+	for (auto& resource : resources.sampled_images)
+	{
+        std::pair<std::string, std::string> unif;
+
+		spirv_cross::SPIRType type = compiler.get_type(resource.base_type_id);
+
+		unif.first = compiler.get_name(resource.id);
+		uint32_t set = compiler.get_decoration(resource.id, spv::DecorationDescriptorSet);
+		uint32_t binding = compiler.get_decoration(resource.id, spv::DecorationBinding);
+
+		// todo
+        unif.second = "smapler";
+
+		uniforms.push_back(unif);
+	}
+
+	//for (auto& resource : resources.storage_images)
+	//{
+	//	ImageUnit img;
+
+	//	spirv_cross::SPIRType type = compiler.get_type(resource.base_type_id);
+
+	//	img.name = compiler.get_name(resource.id);
+	//	uint32_t set = compiler.get_decoration(resource.id, spv::DecorationDescriptorSet);
+	//	img.unit = compiler.get_decoration(resource.id, spv::DecorationBinding);
+	//	img.fmt = parse_image_format(type.image.format);
+	//	img.access = parse_image_access(type.image.access);
+
+	//	m_images.push_back(img);
+
+	//	Variable unif;
+	//	unif.name = img.name;
+	//	unif.type = VariableType::Sampler2D;
+	//	m_uniforms.push_back(unif);
+	//}
+}
+
+void get_shader_uniforms(const char* stage_str, const char* shader_str, const char* lang_str,
+                         std::vector<std::pair<std::string, std::string>>& uniforms)
+{
+    EShLanguage stage;
+    shadertrans::ShaderStage st_stage;
+    if (strcmp(stage_str, "vertex") == 0) {
+        stage = EShLangVertex;
+        st_stage = shadertrans::ShaderStage::VertexShader;
+    } else if (strcmp(stage_str, "tess_ctrl") == 0) {
+        stage = EShLangTessControl;
+        st_stage = shadertrans::ShaderStage::TessCtrlShader;
+    } else if (strcmp(stage_str, "tess_eval") == 0) {
+        stage = EShLangTessEvaluation;
+        st_stage = shadertrans::ShaderStage::TessEvalShader;
+    } else if (strcmp(stage_str, "pixel") == 0) {
+        stage = EShLangFragment;
+        st_stage = shadertrans::ShaderStage::PixelShader;
+    } else if (strcmp(stage_str, "compute") == 0) {
+        stage = EShLangCompute;
+        st_stage = shadertrans::ShaderStage::ComputeShader;
+    } else {
+        return;
+    }
+
+    std::vector<unsigned int> spirv;
+    if (strcmp(lang_str, "glsl") == 0) {
+        shadertrans::ShaderTrans::GLSL2SpirV(st_stage, shader_str, spirv);
+    } else if (strcmp(lang_str, "hlsl") == 0) {
+        shadertrans::ShaderTrans::HLSL2SpirV(st_stage, shader_str, spirv);
+    }
+
+    if (!spirv.empty()) {
+        get_shader_uniforms(stage, spirv, uniforms);
+    }
+}
+
+void w_Render_getShaderUniforms()
+{
+    const char* stage = ves_tostring(1);
+    const char* code  = ves_tostring(2);
+    const char* lang  = ves_tostring(3);
+
+    std::vector<std::pair<std::string, std::string>> uniforms;
+    get_shader_uniforms(stage, code, lang, uniforms);
+
+    ves_pop(4);
+    ves_newlist(static_cast<int>(uniforms.size()));
+    for (int i = 0, n = static_cast<int>(uniforms.size()); i < n; ++i)
+    {
+        auto& unif = uniforms[i];
+
+        int size = 2;
+        if (unif.second == "float4") {
+            size += 4;
+        }
+
+        ves_newlist(size);
+
+        // name
+        ves_pushstring(unif.first.c_str());
+        ves_seti(-2, 0);
+        ves_pop(1);
+
+        // type
+        ves_pushstring(unif.second.c_str());
+        ves_seti(-2, 1);
+        ves_pop(1);
+
+        // values
+        if (unif.second == "float4") {
+            for (int i = 0; i < 4; ++i) {
+                ves_pushnumber(0);
+                ves_seti(-2, 2 + i);
+                ves_pop(1);
+            }
+        }
+
+        ves_seti(-2, i);
+        ves_pop(1);
+    }
+}
+
+void w_Shader_setUniformValue()
+{
+    std::shared_ptr<ur::ShaderProgram> prog = *(std::shared_ptr<ur::ShaderProgram>*)ves_toforeign(0);
+
+    assert(ves_type(1) == VES_TYPE_LIST);
+
+    ves_geti(1, 0);
+    const char* name = ves_tostring(-1);
+    ves_pop(1);
+
+    ves_geti(1, 1);
+    const char* type = ves_tostring(-1);
+    ves_pop(1);
+
+    if (strcmp(type, "float4") == 0)
+    {
+        float val[4];
+        for (int i = 0; i < 4; ++i)
+        {
+            ves_geti(1, 2 + i);
+            val[i] = (float)ves_tonumber(-1);
+            ves_pop(1);
+        }
+
+        auto unif = prog->QueryUniform(name);
+        unif->SetValue(val, 4);
+    }
+}
+
 }
 
 namespace tt
@@ -226,6 +499,9 @@ VesselForeignMethodFn RenderBindMethod(const char* signature)
 {
     if (strcmp(signature, "static Render.draw(_,_,_,_)") == 0) return w_Render_draw;
     if (strcmp(signature, "static Render.clear(_,_)") == 0) return w_Render_clear;
+    if (strcmp(signature, "static Render.getShaderUniforms(_,_,_)") == 0) return w_Render_getShaderUniforms;
+
+    if (strcmp(signature, "Shader.setUniformValue(_)") == 0) return w_Shader_setUniformValue;
 
     return NULL;
 }
