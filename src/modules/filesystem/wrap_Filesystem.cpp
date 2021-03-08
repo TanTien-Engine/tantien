c678b0ad5984a2f2145fb5cb1792315ba6af3d10
zz <xzrunner@gmail.com>
2021-03-09 05:50:05 +0800

    [ADDED] get_absolute_path(path)

--- src/modules/filesystem/wrap_Filesystem.cpp
+++ src/modules/filesystem/wrap_Filesystem.cpp
@@ -1,6 +1,8 @@
 #include "modules/filesystem/wrap_Filesystem.h"
 #include "modules/filesystem/Filesystem.h"
 
+#include <filesystem>
+
 #include <string.h>
 
 namespace
@@ -25,6 +27,15 @@ void w_Filesystem_setAssetBaseDir()
     tt::Filesystem::Instance()->SetAssetBaseDir(directory);
 }
 
+void w_Filesystem_get_absolute_path()
+{
+    const char* path = ves_tostring(1);
+    const auto& dir = std::filesystem::path(tt::Filesystem::Instance()->GetAssetBaseDir());
+    auto absolute = std::filesystem::canonical(std::filesystem::path(dir) / path).string();
+
+    ves_set_lstring(0, absolute.c_str(), absolute.size());
+}
+
 }
 
 namespace tt
@@ -33,6 +44,7 @@ namespace tt
 VesselForeignMethodFn FilesystemBindMethod(const char* signature)
 {
     if (strcmp(signature, "static Filesystem.setAssetBaseDir(_)") == 0) return w_Filesystem_setAssetBaseDir;
+    if (strcmp(signature, "static Filesystem.get_absolute_path(_)") == 0) return w_Filesystem_get_absolute_path;
 
     return NULL;
 }
