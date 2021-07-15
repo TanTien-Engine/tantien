567e934c34f0e80f9c7a526c1e8b7c382c6a7b5c
zz <xzrunner@gmail.com>
2021-07-15 17:43:27 +0800

    [ADDED] BeginContact callback

--- src/modules/physics/Physics.cpp
+++ src/modules/physics/Physics.cpp
@@ -1,9 +1,38 @@
 #include "modules/physics/Physics.h"
 #include "modules/graphics/Graphics.h"
 #include "modules/graphics/SpriteRenderer.h"
+#include "modules/script/Proxy.h"
 
 #include <uniphysics/rigid/Factory.h>
 #include <uniphysics/rigid/DebugDraw.h>
+#include <uniphysics/rigid/box2d/World.h>
+#include <uniphysics/rigid/box2d/Body.h>
+
+#include <vessel.h>
+
+namespace
+{
+
+auto begin_contact = [](const std::shared_ptr<up::rigid::box2d::Body>& a, const std::shared_ptr<up::rigid::box2d::Body>& b)
+{
+	auto offset = ves_argnum();
+
+	ves_pushnil();
+	ves_pushnil();
+	ves_import_class("physics", "Body");
+
+	auto proxy_a = (tt::Proxy<up::rigid::box2d::Body>*)ves_set_newforeign(offset + 0, offset + 2, sizeof(tt::Proxy<up::rigid::box2d::Body>));
+	proxy_a->obj = a;
+	auto proxy_b = (tt::Proxy<up::rigid::box2d::Body>*)ves_set_newforeign(offset + 1, offset + 2, sizeof(tt::Proxy<up::rigid::box2d::Body>));
+	proxy_b->obj = b;
+
+	ves_pop(1);
+
+	ves_pushstring("physics_begin_contact(_,_)");
+	ves_call(2, 0);
+};
+
+}
 
 namespace tt
 {
@@ -16,6 +45,10 @@ Physics::Physics()
 
 	auto& pt = m_debug_draw->GetPainter();
 	pt.SetPalette(tt::Graphics::Instance()->GetSpriteRenderer()->GetPalette());
+
+	up::rigid::box2d::World::Callback cb;
+	cb.begin_contact = begin_contact;
+	up::rigid::box2d::World::RegisterCallback(cb);
 }
 
 Physics::~Physics()
