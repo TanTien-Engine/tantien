621bb46a3204cb31ff5245fc3cd5a1b62361ab26
zz <xzrunner@gmail.com>
2025-12-18 07:04:38 +0800

    rm cgal

--- thirdparty/CGAL-5.3.1/CGALConfigVersion.cmake
+++
@@ -1,1 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/lib/cmake/CGAL/CGALConfigVersion.cmake)
--- thirdparty/CGAL-5.3.1/cmake_out/CGALConfig.cmake
+++
@@ -1,5 +0,0 @@
-# 
-# This file points to the CGALConfig.cmake for header-only CGAL.
-#
-
-include(D:/projects/tantien/thirdparty/CGAL-5.3.1/lib/cmake/CGAL/CGALConfig.cmake)
